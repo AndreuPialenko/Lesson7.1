6dc4c2734e8d64eb4d5f19843f3f9d715637a42a
Andreu <a.pialenko@gmail.com>
2018-11-07 21:10:09 +0300

    +1

---
+++ src/ExceptionMain.java
@@ -0,0 +1,22 @@
+public class ExceptionMain {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5};
+        m2(array, 10);
+
+    }
+
+    public static void m1(int[] array, int n){
+        System.out.println(array[n]);
+    }
+
+    public static void m2(int[] array, int n){
+        try {
+            System.out.println(array[n]);
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("ячейки " +n+ " не существуе, выход за пределы массива");
+
+        }finally {
+            System.out.println("m2 выполнен");
+        }
+    }
+}
